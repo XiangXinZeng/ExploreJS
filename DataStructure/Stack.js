e27006df4d456e2e67ca20c832b5469ea99d5220
XiangXinZeng <zxxyb@hotmail.com>
2020-06-21 19:07:55 -0700

    Data Structure - Stack

--- DataStructure/Stack.js
+++ DataStructure/Stack.js
@@ -1,68 +1,61 @@
 //stack functions: push, pop, peek, size/length
 
 //1. Array is a stack
+/*
+function isPalindrome(word){
 
-// function isPalindrome(word){
+    let letters =[];
+    let rword = "";
 
-//     let letters =[];
-//     let rword = "";
+    for(let i = 0; i < word.length; i++){
+        letters.push(word[i]);
+    }
 
-//     for(let i = 0; i < word.length; i++){
-//         letters.push(word[i]);
-//     }
+    for(let i = 0; i < word.length; i++){
+        rword +=  letters.pop();
+    }
 
-//     for(let i = 0; i < word.length; i++){
-//         rword +=  letters.pop();
-//     }
+    return word === rword;
+}
 
-//     return word === rword;
-// }
-
-// console.log(isPalindrome("racecar")); //true
-// console.log(isPalindrome("windows")); //false
+console.log(isPalindrome("racecar")); //true
+console.log(isPalindrome("windows")); //false
+*/
 
 //2. Prototypical stack
+/*
+function Stack(){
+    this.count = 0;
+    this.storage = {};
+}
 
-// function Stack(){
-//     this.count = 0;
-//     this.storage = {};
-// }
+Stack.prototype.push = function(value){
+    this.storage[this.count] = value;
+    this.count++;
+};
 
-// Stack.prototype.push = function(value){
-//     this.storage[this.count] = value;
-//     this.count++;
-// };
+Stack.prototype.pop = function(){
+    if(this.count === 0){
+        return undefined;
+    }
 
-// Stack.prototype.pop = function(){
-//     if(this.count === 0){
-//         return undefined;
-//     }
+    this.count--;
+    let result = this.storage[this.count];
+    delete this.storage[this.count];
 
-//     this.count--;
-//     let result = this.storage[this.count];
-//     delete this.storage[this.count];
+    return result;
+};
 
-//     return result;
-// };
+Stack.prototype.peek = function(){
+    return this.storage[this.count-1];
+};
 
-// Stack.prototype.peek = function(){
-//     return this.storage[this.count-1];
-// };
-
-// Object.defineProperty(Stack.prototype,'size',{
-//     get: function(){
-//         return this.count;
-//     }
-// });
-
-// let s1 = new Stack();
-// s1.push('red');
-// s1.push('green');
-// s1.push('blue');
-// console.log(s1.size); // 3
-// console.log(s1.peek()); // blue
-// console.log(s1.pop()); // blue
-// console.log(s1.pop()); // greeen
+Object.defineProperty(Stack.prototype,'size',{
+    get: function(){
+        return this.count;
+    }
+});
+*/
 
 //3. ES6 Stack class
 
